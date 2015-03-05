14e8f1720870301f4319340e6c470cd7577376f7
Adam Kanari <adam.kanari@poccaDot.com>
2015-03-05 18:48:19 +0200

    first commit

---
+++ Assets/Scripts/TestFairy/UnityTestFairyIntegration.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using TestFairyUnity;
+
+public class UnityTestFairyIntegration : MonoBehaviour {
+	public string tfAPIKey;
+
+	// Use this for initialization
+	void Start () {
+		TestFairy.begin (tfAPIKey);
+	}
+}
